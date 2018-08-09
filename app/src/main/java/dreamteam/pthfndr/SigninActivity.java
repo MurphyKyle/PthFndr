5c33ea80b85031aae4a3e47674bc3e1c70742a08
Kyle Murphy <m.kyle1@yahoo.com>
2018-08-09 17:46:49 -0600

    Added "logo" to the login screen Added other logo pictures to play with Routed application startup to firebase auth ui with the logo

--- app/src/main/java/dreamteam/pthfndr/SigninActivity.java
+++ app/src/main/java/dreamteam/pthfndr/SigninActivity.java
@@ -1,35 +1,19 @@
 package dreamteam.pthfndr;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.location.Location;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
-import com.google.android.gms.maps.model.Polyline;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
-import java.time.LocalDateTime;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
-import dreamteam.pthfndr.models.Path;
-import dreamteam.pthfndr.models.Trip;
-import dreamteam.pthfndr.models.User;
 
 public class SigninActivity extends AppCompatActivity {
     private static final int RC_SIGN_IN = 123;
@@ -38,7 +22,8 @@ public class SigninActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signin);
+        signIn(null);
+        //setContentView(R.layout.activity_signin);
     }
 
     @Override
@@ -73,6 +58,8 @@ public class SigninActivity extends AppCompatActivity {
     }
 
     public void signIn(View view) {
-        startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().setTheme(R.style.Theme_AppCompat_DayNight_DarkActionBar).setAvailableProviders(providers).build(), RC_SIGN_IN);
+        startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder()
+                .setLogo(R.drawable.pth_fndr_logo1png)
+                .setTheme(R.style.Theme_AppCompat_DayNight_NoActionBar).setAvailableProviders(providers).build(), RC_SIGN_IN);
     }
 }
