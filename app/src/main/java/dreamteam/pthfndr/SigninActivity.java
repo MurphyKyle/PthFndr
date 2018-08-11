a7f69d0be366329cc8c0bed85e1e130a9164cb32
Christian Moore <codeyellow97@gmail.com>
2018-08-11 13:48:01 -0600

    Added funtionality that if the user is not in the database, it adds them

--- app/src/main/java/dreamteam/pthfndr/SigninActivity.java
+++ app/src/main/java/dreamteam/pthfndr/SigninActivity.java
@@ -10,9 +10,18 @@ import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import dreamteam.pthfndr.models.User;
 
 
 public class SigninActivity extends AppCompatActivity {
@@ -32,10 +41,27 @@ public class SigninActivity extends AppCompatActivity {
         if (requestCode == RC_SIGN_IN) {
             IdpResponse response = IdpResponse.fromResultIntent(data);
             if (resultCode == RESULT_OK && data != null) {
-                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                final DatabaseReference fDB = FirebaseDatabase.getInstance().getReference();
+                fDB.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot snapshot) {
+                        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                        if (!snapshot.child(user.getUid()).exists()) {
+                            User newUser = new User();
+                            newUser.setName(user.getDisplayName());
+                            newUser.setUID(user.getUid());
+                            Map<String, Object> userValues = newUser.toMap();
+                            Map<String, Object> userUpdates = new HashMap<>();
+                            userUpdates.put("/users/"+newUser.getUID(), userValues);
+                            fDB.updateChildren(userUpdates);
+                        }
+                    }
 
-//                FirebaseDatabase fDB = FirebaseDatabase.getInstance();
-//                DatabaseReference fRef = fDB.getReference("users");
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
 //                User u = new User(user.getDisplayName(), user.getUid());
 //                Trip t = new Trip(null);
 //                t.paths.add(new Path(new Location("bye"), null, null, 50));
--- app/src/main/java/dreamteam/pthfndr/models/User.java
+++ app/src/main/java/dreamteam/pthfndr/models/User.java
@@ -4,11 +4,14 @@ import com.google.firebase.database.Exclude;
 import com.google.firebase.database.IgnoreExtraProperties;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 @IgnoreExtraProperties
 public class User {
 
     private String Name;
+    @Exclude
     private String UID;
     private ArrayList<Trip> Trips = new ArrayList<>();
 
@@ -40,12 +43,19 @@ public class User {
     public void setTrips(ArrayList<Trip> trips) {
         Trips = trips;
     }
-
+    @Exclude
     public String getUID() {
         return UID;
     }
 
     public void setUID(String UID) {
         this.UID = UID;
+    }
+    @Exclude
+    public Map<String, Object> toMap(){
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("Name", Name);
+        result.put("Trips", Trips);
+        return result;
     }
 }
