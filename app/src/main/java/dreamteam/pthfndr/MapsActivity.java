b4e5b7e8d0fe272062bbd129b160378542067464
DESKTOP-U22TG9R\Kent Stringer <stringer_k@hotmail.com>
2018-08-24 12:26:37 -0600

    Decoupled location services from MapsActivity

--- app/src/main/java/dreamteam/pthfndr/MapsActivity.java
+++ app/src/main/java/dreamteam/pthfndr/MapsActivity.java
@@ -37,85 +37,20 @@ import dreamteam.pthfndr.models.User;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
     private static MapsActivity thisRef;
     boolean isActive = false;
-    double latitude = 0;
-    double longitude = 0;
-    Location cLoc;
-    Trip trip = new Trip(Calendar.getInstance().getTime());
     long time = 0;
     private User currentUser;
     private GoogleMap mMap;
-    private final LocationListener locationListener = new LocationListener() {
-        public void onLocationChanged(Location location) {
-            if (isActive) {
-
-                double longitudeNew = location.getLongitude();
-                double latitudeNew = location.getLatitude();
-
-                float currentSpeed = location.getSpeed() * 2.236936F;
-                int width = checkPaths(5, longitudeNew, latitudeNew);
-                Polyline l = null;
-                MPolyLine mp = null;
-                if (currentSpeed <= 10) {
-                    mp = new MPolyLine(longitude, latitude, longitudeNew, latitudeNew, Color.argb(255, 0, 255, 0), width);
-                } else if (currentSpeed >= 11 && currentSpeed <= 30) {
-                    mp = new MPolyLine(longitude, latitude, longitudeNew, latitudeNew, Color.argb(255, 128, 255, 0), width);
-                } else if (currentSpeed >= 31 && currentSpeed <= 60) {
-                    mp = new MPolyLine(longitude, latitude, longitudeNew, latitudeNew, Color.argb(255, 255, 255, 0), width);
-                } else if (currentSpeed >= 61 && currentSpeed <= 90) {
-                    mp = new MPolyLine(longitude, latitude, longitudeNew, latitudeNew, Color.argb(255, 255, 163, 0), width);
-                } else if (currentSpeed >= 91) {
-                    mp = new MPolyLine(longitude, latitude, longitudeNew, latitudeNew, Color.argb(255, 255, 0, 0), width);
-                } else {
-                    mp = new MPolyLine(longitude, latitude, longitudeNew, latitudeNew, Color.argb(255, 0, 255, 0), width);
-
-                }
-                l = mMap.addPolyline(new PolylineOptions()
-                        .add(new LatLng(mp.getStartingLatitude(), mp.getStartingLongitude()), new LatLng(mp.getEndingLatitude(), mp.getEndingLongitude()))
-                        .width(mp.getWidth())
-                        .color(mp.getColor())
-                );
-                trip.paths.add(new Path(new MLocation(cLoc.getSpeed(), latitude, longitude), new MLocation(location.getSpeed(), latitudeNew, longitudeNew), mp, Color.DKGRAY, (int) (System.currentTimeMillis() - time) / 1000));
-                cLoc = location;
-            }
-            longitude = location.getLongitude();
-            latitude = location.getLatitude();
-        }
-
-        private int checkPaths(int i, double longitudeNew, double latitudeNew) {
-            for (Trip t : currentUser.getTrips()) {
-                for (Path p : t.getPaths()) {
-                    if (p.getEndLocation().getLatitude() >= (latitudeNew - .0004) && p.getEndLocation().getLatitude() <= (latitudeNew + .0004) ||
-                            p.getStartLocation().getLatitude() >= (latitudeNew - .0004) && p.getStartLocation().getLatitude() <= (latitudeNew + .0004)) {
-                        if (p.getEndLocation().getLongitude() >= (longitudeNew - .0004) && p.getEndLocation().getLongitude() <= (longitudeNew + .0004) ||
-                                p.getStartLocation().getLongitude() >= (longitudeNew - .0004) && p.getStartLocation().getLongitude() <= (longitudeNew + .0004)) {
-                            i += 5;
-                            i = i > 100 ? 100 : i;
-                        }
-                    }
-                }
-            }
-            return i;
-        }
-
-        @Override
-        public void onStatusChanged(String s, int i, Bundle bundle) {
-        }
-
-        @Override
-        public void onProviderEnabled(String s) {
-        }
-
-        @Override
-        public void onProviderDisabled(String s) {
-        }
-    };
     private boolean mLocationPermissionGranted;
     private DrawerLayout mDrawerLayout;
+    private LocServices ls;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         String[] keys = getIntent().getExtras().keySet().toArray(new String[1]);
         currentUser = getIntent().getExtras().getParcelable(keys[0]);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
@@ -141,11 +76,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
                     return true;
                 });
-    }
+
+    }//keep here
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        ls = new LocServices("locServ", currentUser, mMap);
         if (currentUser != null) {
             for (Trip t : currentUser.getTrips()) {
                 for (Path p : t.getPaths()) {
@@ -179,13 +116,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        cLoc = location;
-        latitude = location.getLatitude();
-        longitude = location.getLongitude();
-        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, locationListener);
+        ls.cLoc = location;
+        ls.latitude = location.getLatitude();
+        ls.longitude = location.getLongitude();
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, ls.getLocationListener());
 
         time = System.currentTimeMillis();
-    }
+    }//keep here
 
     private void getLocationPermission() {
         if (ContextCompat.checkSelfPermission(this.getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION)
@@ -194,7 +131,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         } else {
             ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 1);
         }
-    }
+    }//keep here
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
@@ -213,22 +150,23 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 }
             }
         }
-    }
+    }//keep here
 
     public void signOut(View view) {
         Intent i = new Intent(this, ProfileActivity.class);
         User user = getIntent().getExtras().getParcelable("user");
         i.putExtra("user", user);
         startActivity(i);
-    }
+    }//keep here
 
     public void manageTrip(View view) {
         isActive = !isActive;
+        ls.isActive = isActive;
 
         Button b = findViewById(R.id.TripButton);
         if (isActive) {
-            if (trip == null) {
-                trip = new Trip();
+            if (ls.trip == null) {
+                ls.NewTrip();
             }
             b.setText(R.string.endTrip);
         } else {
@@ -247,9 +185,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     }
                 }
             }
-            trip.endTrip();
-            currentUser.addTrip(trip);
-            trip = new Trip();
+            ls.trip.endTrip();
+            currentUser.addTrip(ls.trip);
+            ls.NewTrip();
             b.setText(R.string.startTrip);
             updateUser();
         }
